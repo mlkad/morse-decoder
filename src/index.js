7a90602015486161a636b005790cf7330f866f73
mlkad <dusmagambetovaaa@gmail.com>
2024-08-16 11:25:28 +0500

    add code

--- src/index.js
+++ src/index.js
@@ -38,9 +38,30 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';
+    
+    for (let i = 0; i < expr.length; i += 10) {
+        let chunk = expr.slice(i, i + 10);
+        
+        if (chunk === '**********') {
+            result += ' ';
+            continue;
+        }
+        
+        let morseCode = '';
+        
+        for (let j = 0; j < 10; j += 2) {
+            let pair = chunk.slice(j, j + 2);
+            if (pair === '10') morseCode += '.';
+            if (pair === '11') morseCode += '-';
+        }
+        
+        result += MORSE_TABLE[morseCode];
+    }
+    
+    return result;
 }
 
 module.exports = {
     decode
-}
+};
